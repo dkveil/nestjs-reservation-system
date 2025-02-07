dad85abc74b4ed4296b3c74dc0aba24ca731ba7e
Damian Kądziela <damiankadziela4@gmail.com>
2025-02-07 23:13:26 +0100

    add swagger documentation api

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ZodFilter } from '@app/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ReservationsModule, {
 		snapshot: process.env.NODE_ENV === 'production',
 	});
 	app.useGlobalFilters(new ZodFilter());
+
+	const config = new DocumentBuilder().setTitle('Reservations').setDescription('Reservations service').setVersion('1.0.0').build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	await app.listen(process.env.PORT ?? 3157);
 }
 bootstrap();
--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters } from '@nestjs/common';
+import { Controller, Post, Body, UseFilters } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
-import { CreateReservationDto } from './dto/create-reservation.dto';
-import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { CreateReservationDto, CreateReservationDtoSwagger } from './dto/create-reservation.dto';
 import { ZodPipe, ZodFilter } from '@app/common';
 import { CreateReservationSchema } from './schema/reservation.schema';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Reservations')
 @Controller('reservations')
 @UseFilters(ZodFilter)
 export class ReservationsController {
 	constructor(private readonly reservationsService: ReservationsService) {}
 
 	@Post()
-	create(@Body(new ZodPipe(CreateReservationSchema)) createReservationDto: CreateReservationDto) {
+	@ApiOperation({ summary: 'Create new reservation' })
+	@ApiResponse({
+		status: 201,
+		description: 'Reservation created successfully',
+		type: CreateReservationDtoSwagger,
+	})
+	@ApiResponse({
+		status: 400,
+		description: 'Invalid input data',
+	})
+	create(
+		@Body(new ZodPipe(CreateReservationSchema))
+		createReservationDto: CreateReservationDto
+	) {
 		return this.reservationsService.create(createReservationDto);
 	}
 }
